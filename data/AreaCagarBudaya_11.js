1ffbfd3594bcf20b6d77ad36c3b82d408a2408ba
ocsanto98-prog <ocsanto98@gmail.com>
2025-10-02 14:15:38 +0800

    Add files via upload

---
+++ data/AreaCagarBudaya_11.js
@@ -0,0 +1,12 @@
+var json_AreaCagarBudaya_11 = {
+"type": "FeatureCollection",
+"name": "AreaCagarBudaya_11",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "043DC8960D3A31D21D88", "Name": "2°17'47.7\"S 121°45'40.8\"E", "description": "<div>Goa Tokandindi<br></div>", "timestamp": null, "begin": null, "end": null, "altitudeMode": null, "tessellate": -1.0, "extrude": 1.0, "visibility": -1.0, "drawOrder": null, "icon": null }, "geometry": { "type": "Point", "coordinates": [ 121.7613333, -2.2965833 ] } },
+{ "type": "Feature", "properties": { "id": "07BB39CC733A31D446F8", "Name": "2°17'50.2\"S 121°45'38.6\"E", "description": "<div>Objek Diduga Cagar Budaya (ODCB) Ceruk Puumboto<br></div>", "timestamp": null, "begin": null, "end": null, "altitudeMode": null, "tessellate": -1.0, "extrude": 1.0, "visibility": -1.0, "drawOrder": null, "icon": null }, "geometry": { "type": "Point", "coordinates": [ 121.7607222, -2.2972778 ] } },
+{ "type": "Feature", "properties": { "id": "0EEAA21BC83A31D59278", "Name": "2°17'52.0\"S 121°45'38.9\"E", "description": "<div>Objek Diduga Cagar Budaya (ODCB) Gua Puumboto<br></div>", "timestamp": null, "begin": null, "end": null, "altitudeMode": null, "tessellate": -1.0, "extrude": 1.0, "visibility": -1.0, "drawOrder": null, "icon": null }, "geometry": { "type": "Point", "coordinates": [ 121.7608056, -2.2977778 ] } },
+{ "type": "Feature", "properties": { "id": "0D554B90013A31D70A46", "Name": "2°20'24.2\"S 121°45'43.9\"E", "description": "<div>Objek Diduga Cagar Budaya (ODCB) Vayau atau Gua Kulpini<br></div>", "timestamp": null, "begin": null, "end": null, "altitudeMode": null, "tessellate": -1.0, "extrude": 1.0, "visibility": -1.0, "drawOrder": null, "icon": null }, "geometry": { "type": "Point", "coordinates": [ 121.7622, -2.34005 ] } },
+{ "type": "Feature", "properties": { "id": "0F73069C293A31D8C2D9", "Name": "2°20'28.7\"S 121°45'38.8\"E", "description": "<div>Objek Diduga Cagar Budaya (ODCB) Vayau Ngoyu<br></div>", "timestamp": null, "begin": null, "end": null, "altitudeMode": null, "tessellate": -1.0, "extrude": 1.0, "visibility": -1.0, "drawOrder": null, "icon": null }, "geometry": { "type": "Point", "coordinates": [ 121.7607778, -2.3413056 ] } }
+]
+}
---
+++ data/kameratrap3_6.js
@@ -0,0 +1,8 @@
+var json_kameratrap3_6 = {
+"type": "FeatureCollection",
+"name": "kameratrap3_6",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "Lokasi Kamera Trap 3", "descriptio": null, "Koordinat": "2°22’15.0\"S 121°43’37.5\"E" }, "geometry": { "type": "Point", "coordinates": [ 121.72709, -2.37083 ] } }
+]
+}
