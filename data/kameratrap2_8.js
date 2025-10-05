19bf9c9f684b146d2a77091126893202f4a69f9f
ocsanto98-prog <ocsanto98@gmail.com>
2025-10-05 11:02:45 +0800

    Add files via upload

---
+++ data/kameratrap2_8.js
@@ -0,0 +1,8 @@
+var json_kameratrap2_8 = {
+"type": "FeatureCollection",
+"name": "kameratrap2_8",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "Lokasi Kamera Trap 2", "descriptio": null, "Koordinat": "2°22’01.8\"S 121°43’43.6\"E" }, "geometry": { "type": "Point", "coordinates": [ 121.72878, -2.36717 ] } }
+]
+}
---
+++ data/kameratrap3_7.js
@@ -0,0 +1,8 @@
+var json_kameratrap3_7 = {
+"type": "FeatureCollection",
+"name": "kameratrap3_7",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "Lokasi Kamera Trap 3", "descriptio": null, "Koordinat": "2°22’15.0\"S 121°43’37.5\"E" }, "geometry": { "type": "Point", "coordinates": [ 121.72709, -2.37083 ] } }
+]
+}
